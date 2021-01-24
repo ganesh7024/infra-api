ef766e725adb0fb53fbcb365d0c1c3f40945a373
ganesh7024 <ganz_gi@yahoo.com>
2021-01-25 00:42:05 +0530

    Update and rename delete.php.php to delete.php
    Added delete for Multiple Features

---
+++ delete.php
@@ -0,0 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+require_once ('././config/dbHandler.php');
+
+$layer_name = $_POST['layer_name'];
+$gid = $_POST['gid'];
+
+$delete_query = pg_query(DBCONNECT, "delete from pipe_v5_600dc26036c70 where gid = ANY(Array [$gid])");
+
+if ($delete_query)
+{
+    http_response_code(201);
+    echo json_encode(array(
+        "status" => true,
+        "upload_message" => "The feature has been deleted sucessfully!!!"
+
+    ));
+}
+
+else
+{
+    http_response_code(401);
+    echo json_encode(array(
+        "status" => false,
+        "upload_message" => "Failed to delete the feature!!!"
+
+    ));
+
+}
+
+?>
