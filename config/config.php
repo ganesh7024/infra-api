b71c4233663f18559bffc4d733aa1a4e4a58bedc
ganesh7024 <ganz_gi@yahoo.com>
2021-02-09 15:05:54 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('PORT', '5432');
 define('USER', 'postgres');
-define('PASS', 'tnega');
+define('PASS', '');
 define('DB_NAME', 'hydra');
 define('SECREAT', '5f2b5cdbe5194f10b3241568fe4e2b24');
 define('ROOTDIR', dirname(__DIR__));
@@ -11,4 +11,5 @@ define('WORKSPACE', 'test_topo');
 define('DATASTORE', 'test_topo_store');
 define('GSPASS', 'geoserver');
 define('GSUSER', 'admin');
+define('BIN_location', "E:\Program Files\bin\shp2pgsql");
 ?>	
